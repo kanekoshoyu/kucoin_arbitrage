4516bc73eb4a975dfbdac68d6bf241ecb257e8bc
Sho Kaneko <kanekoshoyu@gmail.com>
2023-04-04 07:47:42 +0100

    task_pub_chance_all_taker_btc_usdt gets coin symbol

--- src/broker/strategy/all_taker_btc_usdt.rs
+++ src/broker/strategy/all_taker_btc_usdt.rs
@@ -2,6 +2,7 @@ use crate::event::{chance::ChanceEvent, orderbook::OrderbookEvent};
 use crate::model::chance::{ActionInfo, ThreeActions};
 use crate::model::order::OrderSide;
 use crate::model::orderbook::FullOrderbook;
+use crate::strings::{merge_symbol, split_symbol};
 use kucoin_rs::tokio::sync::broadcast::{Receiver, Sender};
 use std::sync::{Arc, Mutex};
 
@@ -11,16 +12,42 @@ pub async fn task_pub_chance_all_taker_btc_usdt(
     sender: &mut Sender<ChanceEvent>,
     local_full_orderbook: Arc<Mutex<FullOrderbook>>,
 ) -> Result<(), kucoin_rs::failure::Error> {
+    let base1 = String::from("BTC");
+    let base2 = String::from("USDT");
+    let base_symbol = String::from("BTC-USDT");
     loop {
         let event = receiver.recv().await?;
-        let symbol: String;
-        if let OrderbookEvent::OrderbookChangeReceived((symbol, _)) = event {
-        } else {
-            log::info!("Please retry");
+        let mut coin_opt: Option<String> = None;
+        match event {
+            OrderbookEvent::OrderbookChangeReceived((symbol, delta)) => {
+                if symbol == base_symbol {
+                    continue;
+                }
+                let (coin, _) = split_symbol(symbol).unwrap();
+                coin_opt = Some(coin);
+            }
+            _ => {
+                log::error!("Unrecognised event");
+                continue;
+            }
+        }
+        let coin = coin_opt.unwrap();
+        let ab = merge_symbol(base1.clone(), base2.clone());
+        let ta = merge_symbol(coin.clone(), base1.clone());
+        let tb = merge_symbol(coin.clone(), base2.clone());
+
+        // TODO test below
+        print!("Analysing {ab},{ta},{tb}");
+
+        let mut full_orderbook = local_full_orderbook.lock().unwrap();
+        let abo = (*full_orderbook).get(&ab);
+        let tao = (*full_orderbook).get(&ta);
+        let tbo = (*full_orderbook).get(&tb);
+        if abo.is_none() || tao.is_none() || tbo.is_none() {
             continue;
         }
-        // "symbol" is obtained, get the arbitrage using the local_full_orderbook
 
+        // "symbol" is obtained, get the arbitrage using the local_full_orderbook
         let bbs: ThreeActions = [
             ActionInfo {
                 action: OrderSide::Buy,
--- src/strings.rs
+++ src/strings.rs
@@ -18,3 +18,27 @@ pub fn symbol_to_tuple(ticker: &str) -> Option<(&str, &str)> {
     let n = ticker.find("-")?;
     Some(((&ticker[..n]), (&ticker[(n + 1)..])))
 }
+
+/// Split a symbol like "BTC-USDT" into ("BTC", "USDT")
+/// ```
+/// use kucoin_arbitrage::strings::split_symbol;
+/// let res = split_symbol(String::from("BTC-USDT"));
+/// assert_eq!(res.unwrap(), (String::from("BTC"), String::from("USDT")));
+/// ```
+pub fn split_symbol(symbol: String) -> Option<(String, String)> {
+    let delimiter = "-";
+    let substrings: Vec<String> = symbol.split(delimiter)
+    .map(|s| s.trim().to_string())
+    .collect();
+    Some((substrings[0].clone(),substrings[1].clone()))
+}
+
+/// merge ("BTC", "USDT") intp "BTC-USDT"
+/// ```
+/// use kucoin_arbitrage::strings::merge_symbol;
+/// let res = merge_symbol(String::from("BTC"), String::from("USDT"));
+/// assert_eq!(res, String::from("BTC-USDT"));
+/// ```
+pub fn merge_symbol(coin1: String, coin2: String) -> String {
+    format!("{}-{}", coin1, coin2)
+}

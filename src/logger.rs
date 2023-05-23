cd7405c61e85168532aec311ab9bb8120fca6bcc
Sho Kaneko <kanekoshoyu@gmail.com>
2023-05-23 23:17:04 +0100

    cargo update

--- src/logger.rs
+++ src/logger.rs
@@ -1,13 +1,17 @@
 use chrono::prelude::Local;
 
 pub fn log_init() {
+    // setup time loggers
     let fmt = "%Y_%m_%d_%H_%M_%S";
     let now = Local::now();
     let formatted_date = now.format(fmt).to_string();
+
+    // Output file
     let dir = "log";
     let filename = String::from(dir) + "/" + &formatted_date + ".log";
     let _ = std::fs::create_dir(dir);
 
+    // Terminal logger
     let console_logger = fern::Dispatch::new()
         .format(move |out, message, record| {
             out.finish(format_args!(
@@ -21,12 +25,13 @@ pub fn log_init() {
         .level(log::LevelFilter::Info)
         .chain(std::io::stdout());
 
+    // File logger
     let file_logger = fern::Dispatch::new()
         .format(|out, message, _| out.finish(format_args!("{}", message)))
         .level(log::LevelFilter::Info)
         .chain(fern::log_file(filename).unwrap());
 
-    // TODO make log file generation configurable
+    // TODO add color to the terminal upon the release of fern 0.7 
     console_logger
         .chain(file_logger)
         .apply()

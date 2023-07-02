1510dafa619db064965b3475efede41694b1d95f
Sho Kaneko <kanekoshoyu@gmail.com>
2023-07-02 16:45:49 +0100

    Update test_kucoin_order_private_latency.rs

--- src/bin/test_kucoin_order_private_latency.rs
+++ src/bin/test_kucoin_order_private_latency.rs
@@ -1,85 +1,84 @@
 /// Test latency between order and private channel order detection
-extern crate kucoin_api;
-use chrono::prelude::Local;
-use kucoin_api::failure;
-use kucoin_api::futures::TryStreamExt;
+/// place extreme order in REST, receive extreme  order in private channel
 use kucoin_api::{
     client::{Kucoin, KucoinEnv},
-    model::websocket::{KucoinWebsocketMsg, WSTopic, WSType},
+    model::websocket::{WSTopic, WSType},
 };
-use kucoin_arbitrage::model::order::OrderSide;
-use kucoin_arbitrage::translator::traits::OrderBookChangeTranslator;
-use uuid::Uuid;
+use kucoin_arbitrage::broker::orderchange::kucoin::task_pub_orderchange_event;
+use kucoin_arbitrage::broker::symbol::filter::symbol_with_quotes;
+use kucoin_arbitrage::broker::symbol::kucoin::{format_subscription_list, get_symbols};
+use kucoin_arbitrage::event::{order::OrderEvent, orderchange::OrderChangeEvent};
+use kucoin_arbitrage::model::counter::Counter;
+use kucoin_arbitrage::{
+    broker::order::kucoin::task_place_order,
+    model::order::{LimitOrder, OrderSide, OrderType},
+};
+use std::{sync::Arc, time::Duration};
+use tokio::sync::broadcast::channel;
+use tokio::sync::Mutex;
+use tokio::time::sleep;
 
-/// main function
 #[tokio::main]
-async fn main() -> Result<(), failure::Error> {
-    // provide logging format
+async fn main() -> Result<(), kucoin_api::failure::Error> {
+    // Provides logging format
     kucoin_arbitrage::logger::log_init();
-    log::info!("Testing Kucoin REST-to-WS latency");
+    log::info!("Log setup");
+
+    // Declares all the system counters
+    let order_counter = Arc::new(Mutex::new(Counter::new("order")));
+
+    // Credentials
     let credentials = kucoin_arbitrage::global::config::credentials();
-    log::info!("{credentials:#?}");
-    // Initialize the Kucoin API struct
     let api = Kucoin::new(KucoinEnv::Live, Some(credentials))?;
-    let url = api.get_socket_endpoint(WSType::Public).await?;
+    let url = api.clone().get_socket_endpoint(WSType::Public).await?;
+    log::info!("Credentials setup");
+
+    // Gets all symbols concurrently
+    let symbol_list = get_symbols(api.clone()).await;
+    log::info!("Total exchange symbols: {:?}", symbol_list.len());
+
+    // Filters with either btc or usdt as quote
+    let symbol_infos = symbol_with_quotes(&symbol_list, "BTC", "USDT");
+    log::info!("Total symbols in scope: {:?}", symbol_infos.len());
+
+    // Changes a list of SymbolInfo into a 2D list of WSTopic per session in max 100 index
+    let subs = format_subscription_list(&symbol_infos);
+    log::info!("Total orderbook WS sessions: {:?}", subs.len());
+
+    // Creates broadcast channels
+    // for placing order
+    let (tx_order, rx_order) = channel::<OrderEvent>(16);
+    // for getting private order changes
+    let (tx_orderchange, _rx_orderchange) = channel::<OrderChangeEvent>(128);
+    log::info!("Broadcast channels setup");
+
+    // TODO use the tx_order to send orders
+    tokio::spawn(task_place_order(
+        rx_order,
+        api.clone(),
+        order_counter.clone(),
+    ));
+
+    // Subscribes private order change websocket
     let mut ws = api.websocket();
+    ws.subscribe(url.clone(), vec![WSTopic::TradeOrders])
+        .await?;
+    tokio::spawn(task_pub_orderchange_event(ws, tx_orderchange));
 
-    let subs = vec![WSTopic::OrderBook(vec!["BTC-USDT".to_string()])];
-    // extreme order
-    let id: Uuid = Uuid::new_v4();
-    let test_symbol: &str = "BTC-USDT";
-    let test_price: f64 = 1.0; // buying BTC at 1 USD, which cannot happen as of 2023
-    let test_volume: f64 = 0.1;
-
-    let dt_order_placed = Local::now();
-
-    api.cancel_all_orders(None, None).await.unwrap();
-    // TODO set a valid limit order
-    api.post_limit_order(
-        id.to_string().as_str(),
-        test_symbol,
-        OrderSide::Buy.as_ref(),
-        test_price.to_string().as_str(),
-        test_volume.to_string().as_str(),
-        None,
-    )
-    .await?;
-    log::info!("Order placed {dt_order_placed}");
-    ws.subscribe(url, subs).await?;
-
-    log::info!("Async polling");
-    let serial = 0;
-    while let Some(msg) = ws.try_next().await? {
-        match msg {
-            KucoinWebsocketMsg::OrderBookMsg(msg) => {
-                let (symbol, data) = msg.data.to_internal(serial);
-                // match symbol
-                if symbol.ne(test_symbol) {
-                    continue;
-                }
-                // BTC-USDT now, check bid volume
-                if data
-                    .bid
-                    .get(&ordered_float::OrderedFloat(test_price))
-                    .is_some()
-                {
-                    // price
-                    log::info!("data: {:#?}", data);
-                    // volume might not be equal, as they are cumulative with other previous orders
-
-                    let dt_order_reported = Local::now();
-                    let delta = dt_order_reported - dt_order_placed;
-                    log::info!("REST-to-WS: {}ms", delta.num_milliseconds());
-                    // I generally get around 2.4s to 3.0s
-                    return Ok(());
-                }
-            }
-            KucoinWebsocketMsg::PongMsg(_) => continue,
-            KucoinWebsocketMsg::WelcomeMsg(_) => continue,
-            _ => {
-                panic!("unexpected msgs received: {msg:?}")
-            }
+    log::info!("All application tasks setup");
+    loop {
+        // use tx_order.send(); to send extreme order
+        let event = OrderEvent::PostOrder(LimitOrder {
+            id: 0,
+            order_type: OrderType::Limit,
+            side: OrderSide::Sell,
+            symbol: "BTC-USDT".to_string(),
+            amount: 1.to_string(),
+            price: 100000.0.to_string(),
+        });
+        if let Err(e) = tx_order.send(event) {
+            log::error!("{e}");
         }
+        sleep(Duration::from_secs(10)).await;
     }
-    Ok(())
 }

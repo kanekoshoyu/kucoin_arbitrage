6b9d3ccfc8538b31ad519192be72ab6a24ce6200
Sho Kaneko <kanekoshoyu@gmail.com>
2024-02-11 15:32:38 +0900

    copy the log set up code

--- src/logger.rs
+++ src/logger.rs
@@ -1,67 +1,96 @@
-use chrono::prelude::Local;
-use colored::Colorize;
-use eyre::Result;
-use fern;
+use core::fmt::Result as FmtResult;
+use eyre::*;
+use serde::*;
+use std::path::Path;
+use std::str::FromStr;
+use std::sync::Arc;
+use tracing::level_filters::LevelFilter;
+use tracing::{Event, Level, Subscriber};
+use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
+use tracing_subscriber::fmt::{format, FmtContext, FormatEvent, FormatFields};
+use tracing_subscriber::registry::LookupSpan;
+use tracing_subscriber::{EnvFilter, Layer};
 
-// TODO find a way to obtain the running binary name for
-// - terminal log whitelisting
-// - console log filename
-
-fn dispatch_console() -> fern::Dispatch {
-    fern::Dispatch::new()
-        .level(tracing::LevelFilter::Info)
-        .format(move |out, message, record| {
-            out.finish(format_args!(
-                "[{}][{}][{}] {}",
-                Local::now().format("%Y-%m-%d %H:%M:%S"),
-                match record.level() {
-                    tracing::Level::Error => "ERROR".red(),
-                    tracing::Level::Warn => "WARN".yellow(),
-                    tracing::Level::Info => "INFO".green(),
-                    tracing::Level::Debug => "DEBUG".cyan(),
-                    tracing::Level::Trace => "TRACE".blue(),
-                },
-                record.target(),
-                message
-            ))
-        })
-        .chain(std::io::stdout())
+#[derive(Debug, Clone, Deserialize)]
+pub struct LogFileConfig {
+    pub directory: String,
+    pub file_name_prefix: String,
+    pub log_level: Level,
 }
 
-fn dispatch_file(filename: &str) -> fern::Dispatch {
-    fern::Dispatch::new()
-        .level(tracing::LevelFilter::Info)
-        .format(move |out, message, record| {
-            out.finish(format_args!(
-                "[{}][{}][{}] {}",
-                Local::now().format("%Y-%m-%d %H:%M:%S"),
-                match record.level() {
-                    tracing::Level::Error => "ERROR",
-                    tracing::Level::Warn => "WARN",
-                    tracing::Level::Info => "INFO",
-                    tracing::Level::Debug => "DEBUG",
-                    tracing::Level::Trace => "TRACE",
-                },
-                record.target(),
-                message
-            ))
-        })
-        .chain(fern::log_file(filename).expect("Failed to create log file"))
+pub fn env_filter_level(log_level: LogLevel) -> EnvFilter {
+    EnvFilter::from_default_env().add_directive(Level::from(log_level).into())
 }
 
-pub fn log_init() -> Result<()> {
-    // setup time loggers
-    let fmt = "%Y_%m_%d_%H_%M_%S";
-    let formatted_date = Local::now().format(fmt).to_string();
-    // Output file
-    let dir = "log";
-    let filename = format!("{dir}/{formatted_date}.log");
-    if std::fs::metadata(dir).is_err() {
-        std::fs::create_dir(dir)?;
+// custom formatter for file log
+struct MyFormatter;
+impl<S, N> FormatEvent<S, N> for MyFormatter
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+    N: for<'a> FormatFields<'a> + 'static,
+{
+    fn format_event(
+        &self,
+        ctx: &FmtContext<'_, S, N>,
+        mut writer: format::Writer<'_>,
+        event: &Event<'_>,
+    ) -> FmtResult {
+        // Format values from the event's's metadata:
+        let metadata = event.metadata();
+        let level = *metadata.level();
+        let datetime = chrono::Utc::now().timestamp() as u64;
+        let target = metadata.target();
+        let thread = std::thread::current();
+        let thread = thread.name().unwrap_or_else(|| "unnamed");
+        let line = metadata.line().unwrap_or_default();
+        write!(
+            writer,
+            "\n[{datetime}][{level}][{thread}][{target}][{line}]"
+        )
+        .unwrap();
+        ctx.format_fields(writer, event)?;
+        Ok(())
     }
-    fern::Dispatch::new()
-        .chain(dispatch_file(&filename))
-        .chain(dispatch_console())
-        .apply()?;
-    Ok(())
+}
+/// daily rolling log file
+pub fn non_blocking_make_writer_file(
+    directory: impl AsRef<Path>,
+    file_name_prefix: impl AsRef<Path>,
+) -> (NonBlocking, WorkerGuard) {
+    let file_appender = tracing_appender::rolling::daily(directory, file_name_prefix);
+    tracing_appender::non_blocking(file_appender)
+}
+
+pub fn setup_composite_logs(
+    log_level: Level,
+    log_file_config: &LogFileConfig,
+) -> Result<WorkerGuard> {
+    use tracing_subscriber::fmt;
+    use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
+    use tracing_subscriber::Registry;
+
+    let (nb, wg) = non_blocking_make_writer_file(
+        &log_file_config.directory,
+        &log_file_config.file_name_prefix,
+    );
+    // file layer with custom formatter
+    let file_layer = fmt::layer()
+        .with_writer(nb)
+        .event_format(MyFormatter)
+        .with_ansi(false)
+        .with_filter(env_filter_level(log_file_config.log_level));
+    // terminal layer
+    let stdout_layer = fmt::layer()
+        .with_writer(std::io::stdout)
+        .with_thread_names(true)
+        .with_line_number(true)
+        .without_time()
+        .with_filter(env_filter_level(log_level));
+
+    // Combine layers into a single subscriber and set global default
+    let subscriber = Registry::default().with(stdout_layer).with(file_layer);
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to set global default subscriber");
+
+    Ok(wg)
 }

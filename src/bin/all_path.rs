2556055a6e620bc49c9638dcd270a692a592567e
Sho Kaneko <kanekoshoyu@gmail.com>
2024-03-18 01:21:20 +0900

    pair_to_cycle done

--- src/bin/all_path.rs
+++ src/bin/all_path.rs
@@ -23,6 +23,11 @@ pub struct Pair {
     base: u64,
     quote: u64,
 }
+impl Pair {
+    pub fn new(base: u64, quote: u64) -> Self {
+        Pair { base, quote }
+    }
+}
 impl Debug for Pair {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let base = hash_to_string(self.base);
@@ -55,8 +60,8 @@ pub enum Action {
 
 #[derive(Clone, Copy, Hash, PartialEq, PartialOrd, Eq, Ord)]
 pub struct TradeAction {
-    pair: Pair,
-    action: Action,
+    pub pair: Pair,
+    pub action: Action,
 }
 impl Debug for TradeAction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -109,6 +114,13 @@ impl TradeCycle {
     }
     pub fn first(&self) -> Option<&TradeAction> {
         self.actions.first()
+    }
+    pub fn get_all_pairs(&self) -> HashSet<Pair> {
+        let mut pairs = HashSet::<Pair>::new();
+        for action in &self.actions {
+            pairs.insert(action.pair);
+        }
+        pairs
     }
 }
 impl Debug for TradeCycle {
@@ -217,7 +229,9 @@ async fn core(config: kucoin_arbitrage::config::Config) -> Result<()> {
     let symbol_list = symbol_list.expect("failed receiving data from exchange");
     let symbol_list = symbol_list.data.expect("empty symbol list");
     let pairs: Vec<Pair> = symbol_list.into_iter().map(Pair::from).collect();
-    tracing::info!("{}pairs found", pairs.len());
+    tracing::info!("{} pairs found", pairs.len());
+    let dt_found_pairs = chrono::Utc::now();
+
     // usd as starting node
     let start_node = InternedString::from_str("USDT");
     let start_node = start_node.hash().hash();
@@ -226,19 +240,39 @@ async fn core(config: kucoin_arbitrage::config::Config) -> Result<()> {
     // 6 seconds to find cycles len <= 4 (62K cycles)
     // 30 seconds to find cycles len <= 5 (222K cycles)
     // 140 seconds to find cycles len <= 6 (3690K cycles)
-    let length_limit = 3;
+    let length_limit = 4;
     let mut finder = CycleFinder::new(Some(length_limit));
     let found_cycles: Vec<TradeCycle> = finder.find_cycles(pairs, start_node);
     // filter
     let count = |x: &TradeCycle| x.len() >= 3 && x.len() <= length_limit;
     let buy = |x: &TradeCycle| x.first().unwrap().action == Action::Buy;
     let found_cycles: Vec<_> = found_cycles.into_iter().filter(count).filter(buy).collect();
-    if found_cycles.is_empty() {
-        tracing::info!("no cycles found");
-    }
     tracing::info!("{} cycles found", found_cycles.len());
+    let dt_found_cycles = chrono::Utc::now();
 
-    // TODO generate a hash of cycles by pair
+    // TODO currently it is directly storing the cycles into hashmap, which might be too space expensive
+    let mut pair_to_cycle = HashMap::<Pair, Vec<TradeCycle>>::new();
+    for found_cycle in found_cycles {
+        for pair in found_cycle.get_all_pairs() {
+            let cycles = pair_to_cycle.entry(pair).or_default();
+            cycles.push(found_cycle.clone());
+        }
+    }
+    let dt_mapped_cycles = chrono::Utc::now();
+
+    let btc = InternedString::from_str("BTC").hash().hash();
+    let usd = InternedString::from_str("USDT").hash().hash();
+    let new_pair = Pair::new(btc, usd);
+    // these should be the cycles containing BTC_USDT
+    let cycles = pair_to_cycle.entry(new_pair).or_default();
+    for cycle in cycles {
+        tracing::info!("{cycle}");
+    }
+    let dt_found_mapped_cycles = chrono::Utc::now();
+
+    dbg!(dt_found_cycles-dt_found_pairs); //750ms
+    dbg!(dt_mapped_cycles-dt_found_cycles);//2ms
+    dbg!(dt_found_mapped_cycles-dt_mapped_cycles);//4us without print
     Ok(())
 }
 
@@ -250,11 +284,11 @@ mod tests {
     fn test_dfs() {
         // Setup a simple graph that represents the trading pairs.
         let pairs = vec![
-            Pair { base: 1, quote: 2 },
-            Pair { base: 2, quote: 3 },
-            Pair { base: 3, quote: 1 },
-            Pair { base: 2, quote: 4 },
-            Pair { base: 4, quote: 1 },
+            Pair::new(1, 2),
+            Pair::new(2, 3),
+            Pair::new(3, 1),
+            Pair::new(2, 4),
+            Pair::new(4, 1),
         ];
 
         let start_node = 1u64;
@@ -299,11 +333,7 @@ mod tests {
     #[test]
     fn test_dfs_case_2() {
         // Setup a simple graph that represents the trading pairs.
-        let pairs = vec![
-            Pair { base: 1, quote: 2 },
-            Pair { base: 2, quote: 3 },
-            Pair { base: 3, quote: 1 },
-        ];
+        let pairs = vec![Pair::new(1, 2), Pair::new(2, 3), Pair::new(3, 1)];
 
         let start_node = 1u64;
         let mut finder = CycleFinder::new(None);
